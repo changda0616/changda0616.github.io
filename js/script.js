da8216d76123a41a45ad436c477c1096f07fd183
Chad Zhuang <changda0616@gmail.com>
2016-06-06 15:09:10 +0800

    Final

--- js/script.js
+++ js/script.js
@@ -1,39 +1,36 @@
 $(document).ready(function(){
-
-  (function(){
-
-    function newTyped(){ /* A new typed object */ };
-    function foo(){ console.log("Callback"); };
-
-        $("#typed").typed({
-          // strings: ["Hello, my name is Chad.", "Hello, my name is Chad.<br>Please feed me coffee", "And then deletes them.", "Try it out!"],
-          stringsElement: $('#typed-strings'),
-          typeSpeed: 10,
-          backDelay: 400,
-          loop: false,
-          contentType: 'html', // or text
-          // defaults to false for infinite loop
-          loopCount: false,
-          showCursor:true,
-          callback: function(){ foo(); },
-          resetCallback: function() { newTyped(); }
-      });
-
-      $(".reset").click(function(){
-          $("#typed").typed('reset');
-      });
-  })();
-
+  console.log(window.innerHeight);
+  console.log(window.innerWidth);
+  if($(window).innerWidth()>=1211){
+    $("#descri-lg-div img:first-child").toggleClass("coffee");
+    $("#descri-lg-div img:nth-child(2)").toggleClass("selfie-img");
+    $("#descri-lg-div img:nth-child(3)").toggleClass("descri1");
+    $("#descri-lg-div img:nth-child(4)").toggleClass("descri2");
+    $("#descri-lg-div img:nth-child(5)").toggleClass("descri3");
+  }else if($(window).innerWidth()<1211 && $(window).innerWidth()>=994){
+    $("#descri-md-div img:first-child").toggleClass("coffee");
+    $("#descri-md-div img:nth-child(2)").toggleClass("selfie-img");
+    $("#descri-md-div img:nth-child(3)").toggleClass("descri1");
+    $("#descri-md-div img:nth-child(4)").toggleClass("descri2");
+    $("#descri-md-div img:nth-child(5)").toggleClass("descri3");
+  }else if($(window).innerWidth()<994 && $(window).innerWidth()>769){
+    $("#descri-sm-div img:first-child").toggleClass("coffee");
+    $("#descri-sm-div img:nth-child(2)").toggleClass("selfie-img");
+    $("#descri-sm-div img:nth-child(3)").toggleClass("descri1");
+    $("#descri-sm-div img:nth-child(4)").toggleClass("descri2");
+    $("#descri-sm-div img:nth-child(5)").toggleClass("descri3");
+  }else if($(window).innerWidth()<=769){
+    $("#descri-tab-div").show();
+    $("#descri-tab-word").show();
+    $(".aboutme").hide();
+    $("#descri-tab-div img:first-child()").toggleClass("selfie-img");
+    $("#descri-tab-div img:nth-child(2)").toggleClass("descri1");
+    $("#descri-tab-div img:nth-child(3)").toggleClass("descri2");
+    $("#descri-tab-div img:nth-child(4)").toggleClass("descri3");
+  }
   $(".main").onepage_scroll({
     beforeMove: function(index){
       if( index == 2 ){
-/*        $("#imgLayer").animate({top:"50em"},
-        1000,
-        "easeInSine",
-        function(){
-          $(this).hide();
-        });
-*/
         $("#coffee-drip-1").animate({opacity:"100"},2000,"easeInSine");
         $("#html").animate({opacity:"100"},4000,"easeInSine");
         $("#css").animate({opacity:"100"},6000,"easeInSine");
@@ -42,22 +39,64 @@ $(document).ready(function(){
         $("#coffee-drip-2").animate({opacity:"100"},12000,"easeInSine");
       }
       if( index == 3 ){
-        $("#descri1").fadeIn(500,function(){
-          $("#descri2").fadeIn(500,function(){
-              $("#descri3").fadeIn(500);
+        $(".coffee").show();
+          $('.selfie-img').show();
+          $(".descri1").fadeIn(500,function(){
+            $(".descri2").fadeIn(500,function(){
+              $(".descri3").fadeIn(500);
+            });
           });
-        });
-      }
+        }
     }
   });
   (function(){
+        $("#typed").typed({
+          stringsElement: $('#typed-strings'),
+          typeSpeed: 10,
+          backDelay: 400,
+          loop: false,
+          contentType: 'html',
+          loopCount: false,
+          showCursor:true,
+      });
+      $(".reset").click(function(){
+          $("#typed").typed('reset');
+      });
+  })();
+  (function(){
     emailjs.init("user_38Lby2e0A7XlXLZ63GrDJ");
 })();
-
-
   $("#response-form").submit(function(){
     emailjs.sendForm('default_service', 'mail_form', this);
     $("#email-result").css("display","inline-block").fadeIn(1000).fadeOut(2000);
     return false;
   });
+});
+
+$(window).on("resize",function(){
+  if($(this).innerWidth()>=1211){
+    $(".descri-tab").hide();
+    $(".aboutme").show();
+    $(".descri-sm").hide();
+    $(".descri-md").hide();
+    $(".descri-lg").show();
+  }else if($(this).innerWidth() <1211 && $(this).innerWidth() > 994){
+    $(".descri-tab").hide();
+    $(".aboutme").show();
+    $(".descri-sm").hide();
+    $(".descri-md").show();
+    $(".descri-lg").hide();
+  }else if($(this).innerWidth()<=994 && $(this).innerWidth() > 769){
+    $(".descri-tab").hide();
+    $(".aboutme").show();
+    $(".descri-sm").show();
+    $(".descri-md").hide();
+    $(".descri-lg").hide();
+  }else if($(this).innerWidth()<=769){
+    $(".descri-tab").show();
+    $(".aboutme").hide();
+    $(".descri-sm").hide();
+    $(".descri-md").hide();
+    $(".descri-lg").hide();
+  }
 });
---
+++ readme.md
@@ -0,0 +1,16 @@
+五月一日
+  完成：熟悉 media query 語法
+  下階段： 針對使用者頁面load進來時，改變適當page3的圖片大小
+                      還有視窗大小改變時，改變適當page3的圖片大小
+五月二日
+  完成： page3 的 rwd程式碼基礎架構
+  下階段： 針對平板 直放 橫放 改變畫面
+          以及 iphone 5 6 6plus 4吋手機的細節修正 （now focus on page3）
+五月三日
+  完成：page rwd js給定class的程式碼重建
+                利用toggleClass修飾
+  下階段：目前進度40%,欲達成一次偵測頁面後給訂class 並在scroll統一進特效
+五月四日
+  完成：page3 rwd js重建完成 css完成
+（兩點）              
+下階段：page1 2 3 仍須改善
